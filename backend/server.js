b50504d7bc64b0e52620f3b165d274ac46c61709
malena <male.17sole@gmail.com>
2025-05-07 19:39:20 -0300

    guardar22

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.post('/send', async (req, res) => {
     const { nombre, email, mensaje } = req.body;
 
@@ -21,8 +23,9 @@ app.post('/send', async (req, res) => {
 
     try {
         await transporter.sendMail({
-            from: email,
-            to: 'oficina20sanjuan@gmail.com',
+            from: 'oficina20sanjuan@gmail.com', 
+            replyTo: email,                     
+            
             subject: 'Nuevo mensaje desde la web',
             text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`
         });
